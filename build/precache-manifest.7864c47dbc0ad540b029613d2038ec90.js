b1c61699658065dd981362a9f7a93a7d282bbba6
Ryo Kawakami <ryo.kawakami@miidas.jp>
2019-09-20 16:34:07 +0900

    fix

---
+++ build/precache-manifest.7864c47dbc0ad540b029613d2038ec90.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee6d136523516f611606f1668dbdf59a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7709b4e8cca1caa2f32",
+    "url": "/static/css/main.4fcf282f.chunk.css"
+  },
+  {
+    "revision": "ebd3c30581d8d6fa51db",
+    "url": "/static/js/2.137ac224.chunk.js"
+  },
+  {
+    "revision": "b7709b4e8cca1caa2f32",
+    "url": "/static/js/main.c0a49356.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0bd472b20e80e581241ef00f1d476d12.js"
+  "/precache-manifest.7864c47dbc0ad540b029613d2038ec90.js"
 );
 
 self.addEventListener('message', (event) => {
