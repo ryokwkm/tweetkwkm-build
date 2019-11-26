4df1706393c8abe5958bc2b53dba4318b651208c
Ryo Kawakami <ryo.kawakami@miidas.jp>
2019-11-26 15:43:07 +0900

    git

---
+++ git.php
@@ -0,0 +1,26 @@
+<?php
+
+if (!function_exists('getallheaders')) { 
+    function getallheaders() 
+    { 
+           $headers = []; 
+       foreach ($_SERVER as $name => $value) 
+       { 
+           if (substr($name, 0, 5) == 'HTTP_') 
+           { 
+               $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value; 
+           } 
+       } 
+       return $headers; 
+    } 
+} 
+
+
+$header = getallheaders();
+if ( isset($header['X-Hub-Signature']) ) {
+  echo exec('git pull');
+}  else {
+  echo "non";
+}
+
+?>
